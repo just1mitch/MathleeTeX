d5b38041981bd66bf4e4bce0edb630135ca48149
justm1tch <57031880+just1mitch@users.noreply.github.com>
2024-05-11 19:03:52 +0800

    Added extra styling to render questions as buttons. Added extra documentation to render_katex.js

--- app/static/JavaScript/render_katex.js
+++ app/static/JavaScript/render_katex.js
@@ -1,8 +1,9 @@
+// Function to render all KaTeX in elements of the class 'render-katex'
+// Iterates over the text of the element, finds any existence of a \
+// which denotes the start of KaTeX code, and tries to render it
 $('.render-katex').each(function() {
     const element = $(this);
     element.text().split("\\").forEach(function(string) {
-        debugger;
-        console.log("\\" + string)
         try {
             let html = katex.renderToString("\\" + string);
             element.html(element.text().replace("\\" + string, html)); 
