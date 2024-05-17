ea6739c251e1a45caefa6416283edeac21a8d7f3
Nate202003 <23120643@student.uwa.edu.au>
2024-05-18 00:34:58 +0800

    Display login bar upon nav bar redirect
    Also fixed unwanted redirects triggering on paginate button clicks

--- app/static/JavaScript/page_load.js
+++ app/static/JavaScript/page_load.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-    $('a').on('click', function(e) {
+    $('a.nav-link').on('click', function(e) {
     e.preventDefault();
+    localStorage.setItem('lastForm', 'login');
     var url = $(this).attr('href');
     $('#main-content').addClass('content-animation');
     setTimeout(function() {
